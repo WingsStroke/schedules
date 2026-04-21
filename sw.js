d553c001ae790ae7534f4209365fd7b266c3d1c4
WingsStroke <angeltheprolol3@gmail.com>
2026-04-21 14:47:47 -0500

    asjkadj reduciendo dias en excludedays2

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Cambia esta versión cuando subas actualizaciones grandes
-const CACHE_VERSION = 'v2.0.0-beta-44';
+const CACHE_VERSION = 'v2.0.0-beta-47';
 const CACHE_NAME = `horarios-udec-${CACHE_VERSION}`;
 
 const ASSETS_TO_CACHE = [
