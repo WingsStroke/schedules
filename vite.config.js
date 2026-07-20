a39c1b47c03697f025961c5514095ad2b70c7a71
WingsStroke <angeltheprolol3@gmail.com>
2026-07-20 09:51:12 -0500

    fix configuración de vite

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Requerido para que los assets carguen correctamente en GitHub Pages
-  base: '/schedules/',
+  base: '/', //cambiar para pruebas
 });
